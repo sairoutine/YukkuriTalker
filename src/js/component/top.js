5ecbe6975f0c6c1ddef23a8202a564474d681b4f
sairoutine <sairoutine@gmail.com>
2016-07-03 22:38:39 +0900

    add: error message

--- src/js/component/top.js
+++ src/js/component/top.js
@@ -60,7 +60,7 @@ Controller.prototype.onplay = function() {
 			// 再生
 			return self.play_voice_by_binary(binary);
 		}, function(err) {
-			return console.log(err);
+			return self.error("音声データの生成に失敗しました");
 		});
 	};
 };
@@ -94,7 +94,7 @@ Controller.prototype.ondownload = function() {
 			// ダウンロード
 			return self.download_voice_by_binary(binary);
 		}, function(err) {
-			return console.log(err);
+			return self.error("音声データの生成に失敗しました");
 		});
 	};
 };
@@ -208,6 +208,8 @@ Controller.prototype.error = function(text) {
 	var self = this;
 	self.error_message(text);
 	$('#ErrorModal').modal('show');
+	self.is_requesting(false);
+	self.is_playing(false);
 };
 
 
