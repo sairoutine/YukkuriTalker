44496388abb1cb4822a932ad69394b4ddea627ae
sairoutine <sairoutine@gmail.com>
2016-07-09 01:12:38 +0900

    update

--- cmd/src/main.c
+++ cmd/src/main.c
@@ -1,70 +1,53 @@
 #include <stdio.h>
-#include <memory.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <AquesTalk2.h>
+#include "AquesTalk2.h"
 
 int main(int argc,char **argv){
-	// parse arguments
-	int speed=100;
-	if(argc!=3 && argc!=4){
-		printf("usage:\n");
-		printf("  aqwrap <input-file(UTF-8)> <output-file(WAV)> <speed>?\n");
-		return -1;
-	}
-	if(argc==4) speed=atoi(argv[3]);
+    // parse arguments
+    int speed=100;
+    if(argc!=3 && argc!=4){
+        printf("usage:\n");
+        printf("  aqwrap <input-file(SJIS)> <output-file(WAV)> <speed>?\n");
+        return -1;
+    }
+    if(argc==4) speed=atoi(argv[3]);
 
-	// Phont ファイルの読み込み
-	unsigned char *pPhont;
-	FILE *pfi=fopen("./phont/aq_yukkuri.phont","rb");
-	if(pfi==NULL){
-		fprintf(stderr,"couldn't open input file %s\n", "aq_yukkuri.phont");
-		return -2;
-	}
-	fseek(pfi,0,SEEK_END);
-	int pfsize=ftell(pfi);
-	pPhont=malloc(pfsize);
-	fseek(pfi,0,SEEK_SET);
+    // load text
+    unsigned char *text;
+    FILE *fi=fopen(argv[1],"rb");
+    if(fi==NULL){
+        fprintf(stderr,"couldn't open input file %s\n",argv[1]);
+        return -2;
+    }
+    fseek(fi,0,SEEK_END);
+    int fsize=ftell(fi);
+    text=malloc(fsize);
+    fseek(fi,0,SEEK_SET);
 
-	fread(pPhont,pfsize,1,pfi);
-	fclose(pfi);
+    fread(text,fsize,1,fi);
+    fclose(fi);
 
-	// load text
-	unsigned char *text;
-	FILE *fi=fopen(argv[1],"rb");
-	if(fi==NULL){
-		fprintf(stderr,"couldn't open input file %s\n",argv[1]);
-		return -2;
-	}
-	fseek(fi,0,SEEK_END);
-	int fsize=ftell(fi);
-	text=malloc(fsize);
-	fseek(fi,0,SEEK_SET);
+    // feed the text to AquesTalk
+    int size;
+    unsigned char *wav=AquesTalk2_Synthe_Utf8(text,speed,&size, 0);
+    free(text);
 
-	fread(text,fsize,1,fi);
-	fclose(fi);
+    if(wav==NULL){
+        fprintf(stderr,"AquesTalk_Synthe:error:%d\n",size);
+        return -1;
+    }
 
-	// feed the text to AquesTalk
-	int size;
-	unsigned char *wav=AquesTalk2_Synthe_Utf8(text,speed,&size, pPhont);
-	free(text);
+    // write out to a file
+    FILE *fo=fopen(argv[2],"wb");
+    if(fo==NULL){
+        fprintf(stderr,"couldn't open output file %s\n",argv[2]);
+        AquesTalk2_FreeWave(wav);
+        return -2;
+    }
+    fwrite(wav,1,size,fo);
+    AquesTalk2_FreeWave(wav);
+    fclose(fo);
 
-	if(wav==NULL){
-		fprintf(stderr,"AquesTalk_Synthe:error:%d\n",size);
-		return -1;
-	}
-
-	// write out to a file
-	FILE *fo=fopen(argv[2],"wb");
-	if(fo==NULL){
-		fprintf(stderr,"couldn't open output file %s\n",argv[2]);
-		AquesTalk2_FreeWave(wav);
-		return -2;
-	}
-	fwrite(wav,1,size,fo);
-	AquesTalk2_FreeWave(wav);
-	fclose(fo);
-
-	return 0;
+    return 0;
 }
+
