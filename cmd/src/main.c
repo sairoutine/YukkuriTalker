0668dbe26e2212c2b13f16b1ae88e60bcb63fde4
sairoutine <sairoutine@gmail.com>
2016-07-09 01:25:59 +0900

    update

--- cmd/src/main.c
+++ cmd/src/main.c
@@ -33,7 +33,7 @@ int main (int argc, char **argv)
 
 	// AquesTalkにテキストを読ませる
 	int size;
-	unsigned char *wav = AquesTalk2_Synthe(text, speed, &size, 0);
+	unsigned char *wav = AquesTalk2_Synthe_Utf8(text, speed, &size, 0);
 	free(text);
 
 	if (wav == NULL) {
